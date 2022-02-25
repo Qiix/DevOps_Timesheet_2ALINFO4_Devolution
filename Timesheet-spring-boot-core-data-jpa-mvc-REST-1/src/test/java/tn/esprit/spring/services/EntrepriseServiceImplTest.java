4d71bb3d2a49df1a1d13eaf0af77e8c0d1952450
Qiix <gamer135qiix@gmail.com>
2022-02-25 12:06:56 +0100

    adding first unit test: EntrepriseServiceImplTest

---
+++ Timesheet-spring-boot-core-data-jpa-mvc-REST-1/src/test/java/tn/esprit/spring/services/EntrepriseServiceImplTest.java
@@ -0,0 +1,51 @@
+package tn.esprit.spring.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tn.esprit.spring.entities.Entreprise;
+
+public class EntrepriseServiceImplTest {
+	
+	@Autowired
+	IEntrepriseService es;
+
+	@Test
+	public void testAjouterEntreprise() {
+		Entreprise entreprise = new Entreprise("Devolution","DevOps");
+		assertEquals(1,es.ajouterEntreprise(entreprise));
+	}
+
+	@Test
+	public void testAjouterDepartement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAffecterDepartementAEntreprise() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllDepartementsNamesByEntreprise() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEntrepriseById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteDepartementById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEntrepriseById() {
+		fail("Not yet implemented");
+	}
+
+}
